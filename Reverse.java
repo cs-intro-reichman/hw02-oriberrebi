186ba023b6b5e7dbe37811c64810e4ca1c6717a2
OriB <ori.berrebi@runi.ac.il>
2023-12-28 18:40:44 +0200

    HW2

--- Reverse.java
+++ Reverse.java
@@ -4,14 +4,20 @@ class Reverse {
 public static void main(String args[]) {
 
 String input_arg = (args[0]);
-Integer arg_len = (input_arg.length() - 1);
+int arg_len = (input_arg.length() - 1);
 
-while (arg_len>=0){
+while (arg_len >= 0){
 	System.out.print(input_arg.charAt(arg_len));
 		arg_len = arg_len - 1;	
 }
 
-char middle = input_arg.charAt(input_arg.length()/2);
+char middle;
+if (input_arg.length()%2 == 0) {
+	middle = 0
+}
+else {
+	middle = input_arg.charAt(input_arg.length()/2);
+} 
 
 System.out.println();
 System.out.println("The middle character is " + middle);
