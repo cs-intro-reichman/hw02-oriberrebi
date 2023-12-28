89a9ea2e714a3c90e9f2e69171b0f5abd3489bf6
OriB <ori.berrebi@runi.ac.il>
2023-12-28 17:48:46 +0200

    HW2

---
+++ InOrderTest.java
@@ -0,0 +1,19 @@
+import java.util.concurrent.ThreadLocalRandom;
+class InOrder {
+public static void main(String args[]) {
+
+	Integer number = (int) (Math.random()*10);
+	System.out.println(number);
+
+	while (number<10) {
+		Integer i = (int) (Math.random()*10);
+		if (i>= number) {	
+			System.out.println(i);
+			number = i;
+			}
+		else {
+			number=10;
+		}
+	}
+}
+}
