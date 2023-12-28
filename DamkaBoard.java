765d0b3e71d43c8732dd54ddd60e6ed113bf516f
OriB <ori.berrebi@runi.ac.il>
2023-12-28 21:02:20 +0200

    HW2

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -1,5 +1,3 @@
-import java.util.concurrent.ThreadLocalRandom;
-
 class DamkaBoard {
 public static void main(String args[]) {
 
@@ -8,15 +6,30 @@ public static void main(String args[]) {
 
 		for (int i = n; (i> 0); i = (i - 1)) {
 			for (int j = n; (j> 0); j = (j - 1)) {
-				b += "* ";		
+				if (n%2 == 0){
+					if (i%2 == 0){
+						b += "* ";
+					}
+					else {
+						b += " *";
+					}
+                }
+                else {
+                    if (i%2 == 0){
+						b += " *";
+					}
+					else {
+						b += "* ";
+					}
+                }
 			}	
 
-			if (i%2 == 0) {
-				System.out.println(b);
-				}
-		else {
-			System.out.println(" " + b);
-			}
+			//if ((i+j)%2 == 0) {
+			System.out.println(b);
+			//	}
+	//	else {
+			//System.out.println(" " + b);
+		//	}
 		
 		b = "";
 }
